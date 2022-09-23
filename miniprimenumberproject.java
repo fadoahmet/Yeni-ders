feecde736157724a17b8cbfef88a2a78558dfb45
Fadıl Ahmet Tüfekçi <fadoahmet233@gmail.com>
2022-09-23 14:34:25 +0300

    a

--- miniprimenumberproject.java
+++ miniprimenumberproject.java
@@ -1,10 +1,33 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class miniprimenumberproject {
     public static void main(String[] args) {
-        
-        int number = 2;
-    sadasdasds
 
-        
+        int number = 18;
+        boolean isPrime = true;
+
+        if (number == 1) {
+            System.out.println("Sayı asal değildir");
+            return;
+        }
+
+        if (number < 1) {
+            System.out.println("Geçersiz sayı girdiniz");
+        }
+
+        for (int i = 2; i < number; i++) {
+
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            System.out.println("Sayı asaldır");
+
+        } else {
+            System.out.println("Sayı asal değildir");
+        }
+
     }
-    
+
 }
